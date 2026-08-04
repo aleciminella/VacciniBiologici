a3b338b60db1d8f06a556067e036ffb0986ced14
Cosmin3333 <135050898+Cosmin3333@users.noreply.github.com>
2026-08-04 20:48:05 +0200

    Collegamento scenari clinici salvati alla schermata

--- app/src/main/java/it/uninsubria/vaccinibiologici/MainActivity.kt
+++ app/src/main/java/it/uninsubria/vaccinibiologici/MainActivity.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import it.uninsubria.vaccinibiologici.data.AppContainer
 import it.uninsubria.vaccinibiologici.ui.VacciniScreen
 import it.uninsubria.vaccinibiologici.ui.theme.VacciniBiologiciTheme
 
 class MainActivity : ComponentActivity() {
+    private val appContainer by lazy {
+        AppContainer(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             VacciniBiologiciTheme {
-                VacciniScreen()
+                VacciniScreen(
+                    scenarioRepository = appContainer.clinicalScenarioRepository
+                )
             }
         }
     }
